55edee7b2b74c4e7ea217f30b42c0e6f7ae7fa7c
Mateusz Zelent <kkingstoun@gmail.com>
2026-07-29 13:56:51 +0200

    docs: add public documentation foundation

---
+++ public_docs/site/conf.py
@@ -0,0 +1,37 @@
+"""Sphinx configuration for the public FullMag documentation portal."""
+
+from __future__ import annotations
+
+import os
+
+project = "FullMag"
+copyright = "FullMag contributors"
+author = "FullMag contributors"
+release = os.environ.get("FULLMAG_DOCS_VERSION", "latest")
+version = release
+
+extensions = ["myst_parser"]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "internal", "generated"]
+source_suffix = {".md": "markdown", ".rst": "restructuredtext"}
+
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+    "fieldlist",
+    "linkify",
+    "substitution",
+]
+myst_heading_anchors = 3
+
+html_theme = "pydata_sphinx_theme"
+html_title = "FullMag public documentation"
+html_static_path = ["_static"]
+html_theme_options = {
+    "github_url": "https://github.com/MateuszZelent/fullmag",
+    "navbar_align": "content",
+    "show_toc_level": 2,
+    "navigation_with_keys": True,
+}
+
+nitpicky = True
